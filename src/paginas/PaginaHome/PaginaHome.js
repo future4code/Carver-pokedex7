730e650af983cf07527a74956447e6b7c8bac97e
desatinar <rodrigosdn@gmail.com>
2022-01-05 19:11:31 -0300

    pokemons aparecendo na tela

--- src/paginas/PaginaHome/PaginaHome.js
+++ src/paginas/PaginaHome/PaginaHome.js
@@ -3,51 +3,41 @@ import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../../url/url";
 import { Header, CartaoPokemon, ContainerHome } from "./styled";
 
-
 const PaginaHome = () => {
 
   const [pokemon, setPokemon] = useState([])
   const [pokeUrl, setPokeUrl] = useState([])
-  const [imagem, setImagem] = useState([])
 
   // pegar os pokemons na api e setar o nome e a url nos estados
   useEffect(() => {
     axios.get(`${BASE_URL}/pokemon`)
       .then((res) => {
-        // setPokemon(res.data.results)
-        // console.log(res, "primeira resposta")
         setPokemon(res.data.results.map(poke => poke.name))
         setPokeUrl(res.data.results.map(poke => poke.url))
       })
   }, [])
 
-  //tentativa de transformar o objeto url em template
-  const indexUrl = pokeUrl.toString().split('/')[pokeUrl.toString().split('/').length - 2]
-  const imagemUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/20.png`
 
-  // console.log(typeof pokeUrl)
-  // console.log(indexUrl, "index")
-  // console.log(imagemUrl, "imagem")
+  // mapeamento das imagens dos pokemons na tela principal
+  const mapPokeImg = pokeUrl.map(e => {
+    //mostrar a posição onde a string pokemon/ se encontra na url
+    // console.log(e.indexOf("pokemon/"), "indexOf")
 
-  //map das urls
+    //tirar as barras e só exibir o número do pokemon
+    // console.log(e.substring(26 + 8, e.length - 1))
 
-  const indexMap = pokeUrl.map((url) => {
+    //junção das duas numa só variável
+    const indexPokemon = e.substring(e.indexOf("pokemon/") + 8, e.length - 1)
+
     return (
-      <CartaoPokemon>
-        {url}
+      <CartaoPokemon key={indexPokemon}>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${indexPokemon}.png`} />
+        <div>
+          <button onClick={() => console.log(indexPokemon)}>carrinho</button>
+          <button onClick={() => console.log(indexPokemon)}>ver detalhes</button>
+        </div>
       </CartaoPokemon>
     )
-  })
-
-  //loop com a url das imagens, mas não consigo exportar a informação pra usar depois
-
-  pokemon.map(element => {
-    const nomes = element
-    axios.get(`${BASE_URL}/pokemon/${nomes}`)
-      .then((res) => {
-        console.log(res.data.sprites.front_default, "o que eu quero")
-        // setImagem(res.data.sprites.front_default)
-      })
   })
 
   return (
@@ -60,22 +50,12 @@ const PaginaHome = () => {
           LISTA DE POKEMONS
         </p>
       </Header>
-      <img src={imagemUrl} />
-      {indexMap}
       <ContainerHome>
-        <CartaoPokemon>
-          <button>
-            Adicionar a Pokedéx
-          </button>
-          <button>
-            Ver detalhes
-          </button>
-        </CartaoPokemon>
+        {mapPokeImg}
       </ContainerHome>
     </div>
   )
 }
 
 export default PaginaHome
-
 
