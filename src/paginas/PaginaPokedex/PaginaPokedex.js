7f95b4a868c9dd225afa1d3e89d59b1d6b850c33
soraia-aparecida <soraia211norte@gmail.com>
2022-01-05 13:44:48 -0300

    funcionalidade de deletar, ok

--- src/paginas/PaginaPokedex/PaginaPokedex.js
+++ src/paginas/PaginaPokedex/PaginaPokedex.js
@@ -1,25 +1,49 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { goToHome } from '../../routes/coordinatis';
-import { Header, Container } from './styled';
-import CardPokemon from '../../componentes/CardPokedex/CardPokemon';
+import { Header, Container, Card } from './styled';
+ import CardPokemon from '../../componentes/CardPokedex/CardPokemon';
 import { ContextState } from '../../context';
+import { goToDetails } from '../../routes/coordinatis';
 
 export function Pokedex() {
     const history = useHistory()
-    const carriinho = useContext(ContextState)
+    const [carriinho, setCarrinho] = useContext(ContextState)
     console.log(carriinho)
+    console.log("item", carriinho[0].id)
 
-    const mapPokemons = carriinho.map((poke)=> {
-        <CardPokemon
-        // id={}
-        // img={}
-        />
+    
+     const removerPokemon = (pokeId) =>{
+        let remove = carriinho.filter((poke)=>{
+            return pokeId !== poke.id
+            
+        })
+        
+        // console.log("clicou", pokeId)
+        setCarrinho (remove)
+     }
+     
+
+    const mapPokemons = carriinho.map((poke) => {
+        return(
+            <Card key={poke.id}>
+           <p> <img alt="pokemon" src={poke.foto} /></p>
+           <div>
+                <button onClick={() => removerPokemon(poke.id)}>Remover</button>
+                <button onClick={() => goToDetails(history)}>Ver Detalhes</button>
+            </div>
+        </Card>
+            // <CardPokemon
+            // imagem={poke.foto}
+            // id={poke.id}
+            // remover={removerPokemon}
+            // />
+            
+        )
     })
+    console.log("map", mapPokemons)
 
-    // const removerPokemon = 
 
-    const carro = [<CardPokemon />, <CardPokemon />, <CardPokemon />, <CardPokemon />, <CardPokemon />]
     return (
         <div>
             <Header >
@@ -27,18 +51,10 @@ export function Pokedex() {
                 <h1>Pokedex</h1>
             </Header >
             <Container>
-                {/* <CardPokemon />
-                <CardPokemon />
-                <CardPokemon />
-                <CardPokemon />
-                <CardPokemon />
-                <CardPokemon />
-                <CardPokemon />
-                <CardPokemon />
-                <CardPokemon />
-                <CardPokemon /> */}
-                {carro}
-        </Container>
+                
+                {mapPokemons}
+             
+            </Container>
 
         </div>
     )
