e875aa6ba000bacc23a5fd5c84a1f8c4549d3377
Your Name <bruna@fedora.(none)>
2022-01-07 13:39:58 -0300

    styled detailPage complete

--- src/paginas/PaginaDetalhes/styled.js
+++ src/paginas/PaginaDetalhes/styled.js
@@ -9,61 +9,82 @@ export const Container = styled.div`
 
 export const HeaderContainer = styled.header`
     display: flex;
-    border: 2px black solid;
+    border: 2px solid;
+    background-color:  #EE6055;
     max-width: 100vw;
     flex: 1;
     align-items: center;
     justify-content: space-around;
     margin: 5px;
+    border-color: #EE6055;
 `
 
 export const HeaderButton = styled.button`
     padding: 10px;
+    background: ${props => props.primary ? "white":"#FF9B85"};
+    color: ${props => props.primary ? "#FF9B85": "white"};
+    border-color:#FF9B85;
+    border-radius:5px;
 `
 
 export const PokedexButton= styled.button`
     padding: :10px;
+
 `
 
 export const HeaderTitle = styled.p`
-
 `
 
 export const LeftStatusText = styled.p`
 align-self: flex-start;
-margin-left: 10px;
+margin-left: 20px;
 `
 
 export const Content = styled.div`
 display: flex;
-flex: 10;
+flex:10;
 align-content: space-around;
 justify-content: space-evenly;
-
 `
 
 export const FirstContainer = styled.div`
 display: flex;
-flex: 1;
+flex:0.5;
 flex-direction: column;
 align-items: center;
 justify-content: space-around;
-border: 2px solid gray;
 margin: 20px;
+border-color: #FF9B85;
 `
-
-export const ImageContainer = styled.div`
-
+export const FrontImageContainer = styled.div`
+display: flex;
+border: 2px solid;
+border-color: #FF9B85;
+margin: 30px;
+align-items: center;
+flex:1;
+border-radius:10px;
+`
+export const BackImageContainer = styled.div`
+display: flex;
+border: 2px solid;
+border-color: #FF9B85;
+align-items: center;
+flex:1;
+border-radius:10px;
 `
 
 export const SecondContainer = styled.div`
-border: 2px solid gray;
+border: 2px solid;
 display: flex;
-flex: 1;
+flex: 0.5;
 flex-direction: column;
 align-items: center;
 justify-content: center;
 margin: 20px;
+border-color: #FF9B85;
+font-size:30px;
+border-radius:10px;
 `
 
 export const ThirdContainer = styled.div`
@@ -73,18 +94,24 @@ flex: 1;
 `
 
 export const TypeContainer = styled.div`
-border: 2px solid gray;
+border: 2px solid;
 display: flex;
-flex: 1;
+flex: 0.5;
 margin: 20px;
 justify-content: space-around;
+border-color: #FF9B85;
+font-size:25px;
+border-radius:10px;
 `
 export const MovesContainer = styled.div`
-border: 2px solid gray;
+border:2px solid;
 margin: 20px;
 display: flex;
-flex: 10;
+flex: 5;
 flex-direction: column;
 justify-content: center;
 align-items: center;
+border-color: #FF9B85;
+font-size:30px;
+border-radius:10px;
 `
